6defefa86ef789148aa3b520d77ff8e1c9923dfe
Willyan Alves Vieira <willyan.a.vieira@outlook.com>
2024-02-28 14:15:32 -0300

    Methods in C#

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
-﻿// using System;
+﻿// Screen Sound
 
-namespace PrimeiroProjeto
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+Console.WriteLine(mensagemDeBoasVindas);
+
+string firstName = "Willyan";
+string lastName = "Vieira";
+
+firstName = firstName.TrimStart();
+
+string friends =$"My name is {firstName} {lastName}";
+
+Console.WriteLine(friends.StartsWith("My"));
+Console.WriteLine(friends.Length);
--- obj/Debug/net8.0/CursoUdemyC#.AssemblyInfo.cs
+++ obj/Debug/net8.0/CursoUdemyC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoUdemyC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecb0cffdb077061c277c3b0f43e8e0107a71a819")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoUdemyC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoUdemyC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
